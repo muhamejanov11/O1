9463194e08d780993345d26a5b11afa909747048
muhamejanov11 <muhamejanov.n@gmail.com>
2024-04-23 21:56:11 +0500

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+let arr = ["хлеб", "мед", "банан", "яблоко", "Азиза", "цветок", "машина"];
+
+let withO = arr.filter(word => word.toLowerCase().includes('о'));
+
+console.log(arr);
+console.log(withO);
